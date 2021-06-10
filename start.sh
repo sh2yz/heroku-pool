8d774fc786c7385d05fc8cf1fa7fcb5e24f21d33
jth445600 <24479489+jth445600@users.noreply.github.com>
2021-06-11 02:07:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 # download
 # wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/proxypoolv0.7.3 -O proxypool
 # chmod +x proxypool
-wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/source.yaml -O source.yaml
-wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/config.yaml -O config.yaml
-sed  -i "s/port:/port: $PORT/g" config.yaml 
-sed  -i "s/127.0.0.1/$DOMAIN/g" config.yaml 
-wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/assets.zip -O assets.zip
-unzip -d assets assets.zip
+# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/source.yaml -O source.yaml
+# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/config.yaml -O config.yaml
+sed  -i "s/port:/port: $PORT/g" /config.yaml 
+sed  -i "s/127.0.0.1/$DOMAIN/g" /config.yaml 
+# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/assets.zip -O assets.zip
+# unzip -d assets assets.zip
 
-/proxypool -d &
+/proxypoolv0.7.3 -d &
 
