05cd89bf8415eb74b42e717b4331a1e8b6851f3f
jth445600 <24479489+jth445600@users.noreply.github.com>
2021-06-11 01:59:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # download
-wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/proxypoolv0.7.3 -O proxypool
-chmod +x proxypool
+# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/proxypoolv0.7.3 -O proxypool
+# chmod +x proxypool
 wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/source.yaml -O source.yaml
 wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/config.yaml -O config.yaml
 sed  -i "s/port:/port: $PORT/g" config.yaml 
