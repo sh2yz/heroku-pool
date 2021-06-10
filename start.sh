6b0cf443f788fb4eccd6b4dbcb7b096f3cf3cb3a
jth445600 <24479489+jth445600@users.noreply.github.com>
2021-06-11 02:52:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 # chmod +x proxypool
 # wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/source.yaml -O source.yaml
 # wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/config.yaml -O config.yaml
-sed -i "s/port:/port: $PORT/g" /config.yaml 
-sed -i "s/127.0.0.1/$DOMAIN/g" /config.yaml 
+# sed -i "s/port:/port: $PORT/g" /config.yaml 
+# sed -i "s/127.0.0.1/$DOMAIN/g" /config.yaml 
 # wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/assets.zip -O assets.zip
 # unzip -d assets assets.zip
 
