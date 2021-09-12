213533fa4072fdb8c43cf4fbe5720d902c748dde
jth445600 <jth445600@gmail.com>
2021-09-13 03:41:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-/proxypoolv0.7.3 -c /config.yaml &
-# download
-# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/proxypoolv0.7.3 -O proxypool
-# chmod +x proxypool
-# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/source.yaml -O source.yaml
-# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/config.yaml -O config.yaml
-# sed -i "s/port:/port: $PORT/g" /config.yaml 
-# sed -i "s/127.0.0.1/$DOMAIN/g" /config.yaml 
-# wget https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/assets.zip -O assets.zip
-# unzip -d assets assets.zip
+
+    wget -O proxypool-linux-amd64.zip https://github.com/jth445600/hello-world/raw/master/proxypool-linux-amd64/proxypool-linux-amd64.zip
+    unzip proxypool-linux-amd64.zip 
+    chmod +x /proxypoolv0.7.3 
+    sed -i "s/port:/port: $PORT/g" /config.yaml 
+    sed -i "s/127.0.0.1/$DOMAIN/g" /config.yaml 
+    rm -rf /var/cache/apk/* 
+    touch pool.log 
 
 /proxypoolv0.7.3 -c /config.yaml &
 
